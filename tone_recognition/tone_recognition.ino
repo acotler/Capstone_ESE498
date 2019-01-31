895dae5f93fedb4432131cdfb4d87a42fe94ba1b
acotler <42701913+acotler@users.noreply.github.com>
2019-01-31 10:32:25 -0600

    adds lcd to tone_recognition

--- tone_recognition/tone_recognition.ino
+++ tone_recognition/tone_recognition.ino
@@ -1,9 +1,31 @@
+//Libraries
+#include <LiquidCrystal.h>
+
+//Global Variables
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+unsigned long displayTime = 0;
+bool disp = false;
+
 void setup() {
-
-
+  lcd.begin(16, 2);
 }
 
 void loop() {
-
-
+  if(millis()-displayTime >= 1000){
+    lcd.clear();
+    if(disp){
+      lcd.setCursor(0, 0);
+      lcd.print("Tone Detected:");
+      lcd.setCursor(0, 1);
+      lcd.print("Doorbell");
+    }
+    else {
+      lcd.setCursor(0, 0);
+      lcd.print("Rrriiiiinggg");
+      lcd.setCursor(0, 1);
+      lcd.print("Rrriiiiinggg");
+    }
+    disp = !disp;
+    displayTime = millis();
+  }
 }
