f057a02b3481798dc44274cae99d088716f5fa06
acotler <42701913+acotler@users.noreply.github.com>
2019-02-05 17:29:13 -0600

    implements basis for FSM

--- tone_recognition/tone_recognition.ino
+++ tone_recognition/tone_recognition.ino
@@ -11,9 +11,55 @@ int mic = A0;
 //delta timing variables
 unsigned long displayTime = 0;
 bool disp = false;
+unsigned long sampleTime = 0;
 //SD card variables
 File myFile;
+//FSM Variables
+enum State { dataAquisition, FHTProcessing, recordTone };
+State state = dataAquisition;
+bool toneDetected = false;
+bool FHTProcDone = true;
+bool recordTone = false;
+bool recordingTone = false;
 
+//A function to determine state transitions
+State nextState(State currentState){
+  switch(currentState){
+    case dataAquisiton:
+      FHTProcDone = false; //reset FHTProcDone
+      recordingTone = false; //reset recordingTone
+      if(toneDetected) {
+        state = FHTProcessing;
+      }
+      else {
+        state = dataAquistion;
+      }
+
+      if(recordTone){
+        state = recordTone;
+      }
+      break;
+    case FHTProcessing:
+      if(!FHTProcDone) {
+        state = FHTProcessing;
+      }
+      else {
+        state = dataAquisiton;
+        FHTProcDone = true;
+        toneDetected = false;
+      }
+      break;
+    case recordTone:
+      if(recordingTone) {
+        state = recordTone;
+      }
+      else {
+        state = dataAquistion;
+        recordTone = false;
+      }
+      break;
+  }
+}
 
 void setup() {
   Serial.begin(9600); //begin serial connection
@@ -50,7 +96,24 @@ void setup() {
 }
 
 void loop() {
-  if(millis()-displayTime >= 1000){
+  state = nextState(state);
+  
+  switch(state){
+    case dataAquisition: //take 256 samples and see if tone detected
+      if(millis()-sampleTime >= 100){
+        //store sample
+        sampleTime = millis();
+      }
+      //toneDetected = true;
+      break;
+    case FHTProcessing: //Take FHT and compare to files
+      break;       
+    case recordTone: //Record new tones to a file
+      break;
+  }
+  
+  
+  /*if(millis()-displayTime >= 1000){
     lcd.clear();
     if(disp){
       lcd.setCursor(0, 0);
@@ -66,5 +129,11 @@ void loop() {
     }
     disp = !disp;
     displayTime = millis();
-  }
+  }*/
 }
+
+
+
+
+
+  
